1677b9e370cab37efa6513988486c96e8cc98f9b
carmichr <christopher.carmine@cranepi.com>
2023-12-12 08:49:32 -0500

    day 3 part 2 works with sample

--- day3/day3.py
+++ day3/day3.py
@@ -25,10 +25,35 @@ def symbolInRange(schematic, lineNum, charNum):
             
     return False
 
-def isGear(schematic, lineNum, charNum):
-    pass
+def getGearRatio(numArray, row, column):
 
-input = open('day3/day3_0.txt', 'r')
+    rowOffsets = (-1, 0, 1)
+    columnOffsets = (-1, 0, 1)
+    gearOne = 0
+    gearTwo = 0
+
+    for rowOffset in rowOffsets:
+        for columnOffset in columnOffsets:
+            rowToCheck = row+rowOffset
+            columnToCheck = column+columnOffset
+
+            if rowOffset == 0 and columnOffset == 0:
+                continue
+
+            if rowToCheck < 0 or rowToCheck >= len(numArray) or columnToCheck < 0 or columnToCheck > len(numArray[rowToCheck]):
+                continue
+
+            if numArray[rowToCheck][columnToCheck] != '0':
+
+                if gearOne == 0:
+                    gearOne = int(numArray[rowToCheck][columnToCheck])
+                elif gearTwo == 0:
+                    gearTwo = int(numArray[rowToCheck][columnToCheck])
+            
+    return gearOne * gearTwo
+    
+
+input = open('day3/day3_1.txt', 'r')
 schematic = input.readlines()
 
 rows = len(schematic) 
@@ -58,6 +83,9 @@ for (lineNum, line) in enumerate(schematic):
             if char.isdigit():
                 state = State.LOOKING_FOR_END_OF_NUMBER
                 numStart = index
+            # for later gear calc    
+            if char == '*':
+                numArray[lineNum][index] = '*'
 
         if state == State.LOOKING_FOR_END_OF_NUMBER:
             if char.isdigit():
@@ -74,7 +102,7 @@ for (lineNum, line) in enumerate(schematic):
 
                 if isPartNumber == True:
                     sumParts += num
-                    print(f'num: {num}')
+                    #print(f'num: {num}')
                 num = 0
                 exponent = 0
                 state = state = State.LOOKING_FOR_START_OF_NUMBER
@@ -84,5 +112,14 @@ for (lineNum, line) in enumerate(schematic):
 
 print(f'Sum of parts is {sumParts}')
 
-for j in range(0, rows):
-    print(f'{numArray[j]}, length: {len(numArray[j])}')
+# for j in range(0, rows):
+#     print(f'{numArray[j]}, length: {len(numArray[j])}')
+
+gearRatio = 0
+
+for row in range(rows):
+    for col in range(cols):
+        if numArray[row][col] == '*':
+            gearRatio += getGearRatio(numArray, row, col)
+
+print(f'Sum of gear ratios is {gearRatio}')
