65f6d277e0c37a9032773bfbe766a50952ae87c4
dreambucket13 <carmichr@gmail.com>
2023-12-13 23:30:54 -0500

    day 4 starting

---
+++ day4/day4.py
@@ -0,0 +1,28 @@
+# https://adventofcode.com/2023/day/4
+
+import re
+import bisect
+
+input = open('day4/day4_0.txt', 'r')
+games = input.readlines()
+spaceDelimeter = re.compile(r'\s+')
+
+for game in games:
+    gameData = game.split(" | ")
+    winningNumbers = gameData[1]
+    winningNumbersList = spaceDelimeter.split(winningNumbers[:-1])
+    winningNumbersList = list(map(int, winningNumbersList))
+    winningNumbersList.sort()
+
+    myNumbers = gameData[0].split(": ")[1]
+    myNumbersList = spaceDelimeter.split(myNumbers)
+    # TODO handle 1st number having only 1 digit
+    myNumbersList = list(map(int, myNumbersList))
+
+    for number in myNumbersList:
+        i = bisect.bisect_left(winningNumbersList, 6)
+        if i != len(winningNumbersList):
+            #it's in the list
+            pass
+
+    pass
