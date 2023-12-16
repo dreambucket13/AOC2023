c312d67ad6347de72b8f934629ccc80ed50609f3
dreambucket13 <carmichr@gmail.com>
2023-12-16 15:19:00 -0500

    day 4 part 1 working

--- day4/day4.py
+++ day4/day4.py
@@ -3,26 +3,46 @@
 import re
 import bisect
 
-input = open('day4/day4_0.txt', 'r')
+input = open('day4/day4_1.txt', 'r')
 games = input.readlines()
 spaceDelimeter = re.compile(r'\s+')
+colonAndSpaceDelimeter = re.compile(r':\s+')
+totalPoints = 0
+gameNum = 0
 
 for game in games:
     gameData = game.split(" | ")
     winningNumbers = gameData[1]
-    winningNumbersList = spaceDelimeter.split(winningNumbers[:-1])
+    winningNumbersList = spaceDelimeter.split(winningNumbers)
+
+    # sometimes we get an extra space if the 1st or last number is a single digit
+    if winningNumbersList[0] == '':
+        winningNumbersList = winningNumbersList[1:]
+
+    if winningNumbersList[-1] == '':
+        winningNumbersList = winningNumbersList[:-1]
+
     winningNumbersList = list(map(int, winningNumbersList))
     winningNumbersList.sort()
 
-    myNumbers = gameData[0].split(": ")[1]
+    myNumbers = colonAndSpaceDelimeter.split(gameData[0])[1]
     myNumbersList = spaceDelimeter.split(myNumbers)
-    # TODO handle 1st number having only 1 digit
     myNumbersList = list(map(int, myNumbersList))
+
+    matches = 0
 
     for number in myNumbersList:
         i = bisect.bisect_left(winningNumbersList, number)
-        if i < len(winningNumbersList):
-            #it's in the list
-            pass
+        if i < len(winningNumbersList) and i >= 0 and winningNumbersList[i] == number:
+            #print(f'Game {game} matched number {winningNumbersList[i]} at index {i}')
+            matches += 1
 
-    pass
+    points = 0
+    if matches > 0:
+        points = 1 * pow(2, matches - 1)
+        totalPoints += points
+
+    print(f'Game {gameNum} matches {matches}, points: {points}')
+    gameNum += 1
+
+print(f'Total points: {totalPoints}')
