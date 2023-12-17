2cd466df5acffcdc34f85a2508943d8e02b4c845
dreambucket13 <carmichr@gmail.com>
2023-12-16 21:52:18 -0500

    day 4 part 2 working - maybe a bit slow.

--- day4/day4.py
+++ day4/day4.py
@@ -3,12 +3,22 @@
 import re
 import bisect
 
+def getMatches(myNumbersList, winningNumbersList):
+    matches = 0
+    for number in myNumbersList:
+        i = bisect.bisect_left(winningNumbersList, number)
+        if i < len(winningNumbersList) and i >= 0 and winningNumbersList[i] == number:
+            matches += 1
+    return matches
+
 input = open('day4/day4_1.txt', 'r')
 games = input.readlines()
 spaceDelimeter = re.compile(r'\s+')
 colonAndSpaceDelimeter = re.compile(r':\s+')
 totalPoints = 0
-gameNum = 0
+card = 1
+
+parsedCards = []
 
 for game in games:
     gameData = game.split(" | ")
@@ -29,20 +39,41 @@ for game in games:
     myNumbersList = spaceDelimeter.split(myNumbers)
     myNumbersList = list(map(int, myNumbersList))
 
-    matches = 0
-
-    for number in myNumbersList:
-        i = bisect.bisect_left(winningNumbersList, number)
-        if i < len(winningNumbersList) and i >= 0 and winningNumbersList[i] == number:
-            #print(f'Game {game} matched number {winningNumbersList[i]} at index {i}')
-            matches += 1
+    matches = getMatches(myNumbersList, winningNumbersList)
 
     points = 0
     if matches > 0:
         points = 1 * pow(2, matches - 1)
         totalPoints += points
 
-    print(f'Game {gameNum} matches {matches}, points: {points}')
-    gameNum += 1
+    #print(f'Card {card} matches {matches}, points: {points}')
+    card += 1
+
+    parsedCards.append((myNumbersList, winningNumbersList))
 
 print(f'Total points: {totalPoints}')
+
+totalCards = 0
+
+for (index, card) in enumerate(parsedCards):
+    cardsInHand = [index]
+    nextHand = []
+    totalCards += 1
+
+    while len(cardsInHand) > 0:
+        for cardNum in cardsInHand:
+            numbers = parsedCards[cardNum][0]
+            winningNumbers = parsedCards[cardNum][1]
+            matches = getMatches(numbers, winningNumbers)
+            for i in range(1, matches + 1):
+                nextHand.append(cardNum + i)
+                totalCards += 1
+            # print(f'Initial Card: {index}. Processing card {cardNum}, Next: {nextHand}')
+            
+        cardsInHand = nextHand
+        nextHand = []
+
+print(f'Total cards: {totalCards}')
+
+
+    
