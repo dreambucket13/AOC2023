ab60b49eb554b80a3e69aeb4aecafefd1b4f2a12
dreambucket13 <carmichr@gmail.com>
2024-03-23 13:46:53 -0400

    i was told there would be no math

--- day6/day6.py
+++ day6/day6.py
@@ -2,6 +2,7 @@
 
 import re
 from dataclasses import dataclass
+import math
 
 @dataclass
 class RaceData:
@@ -12,14 +13,6 @@ class RaceData:
         velocity = buttonTime
         raceTime = self.time - buttonTime
         distanceTravelled = velocity * raceTime
-
-        #distance travelled = buttonTime * (total time - buttonTime)
-        #                   = buttonTime^2 - buttonTime*totaltime
-
-        # y = distance travelled, z = record, x = buttonTime, a = total race time
-        #                 y - z = x^2 - a*x 
-        #                 y = x^2 - a*x + z
-        # calculate zero intercepts for this equation
 
         return distanceTravelled
     
@@ -72,6 +65,33 @@ def main():
     # Distance:   295   1734   1278   1210
 
     raceData = RaceData(45988373, 295173412781210)
+
+    #distance travelled = buttonTime * (total time - buttonTime)
+    #                   = buttonTime^2 - buttonTime*totaltime
+
+    # y = distance travelled, z = record, x = buttonTime, a = total race time
+    #  we want y - z to be positive.
+    #                 y = ((x) * (a - x)) 
+    #                 y - z = ((x) * (a - x)) - z
+    #                 0 = -x^2 + ax - z
+    #                 quadratic eq where a = -1, b = totalTime, c = record
+    # calculate zero intercepts for this equation
+
+    #     Time:        45     98     83     73
+    # Distance:   295   1734   1278   1210
+
+    a = -1
+    # b = 71530
+    # c = -940200
+    b = 45988373
+    c = -295173412781210
+    x1 = (-1*b + math.sqrt(b*b - 4*a*c))/(2*a)
+    x2 = (-1*b - math.sqrt(b*b - 4*a*c))/(2*a)
+
+    x1 = math.ceil(x1)
+    x2 = math.floor(x2)
+
+    print(f'x1: {x1}, x2: {x2}, total num ways: {x2-x1+1}')
     
 if __name__ == '__main__':
     main()
