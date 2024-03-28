ed7fcb61d6fb6fc31cb561754e2451fa6c722029
dreambucket13 <carmichr@gmail.com>
2024-03-27 22:35:20 -0400

    part 2 example works but very slow

--- day8/day8.py
+++ day8/day8.py
@@ -1,45 +1,78 @@
 # https://adventofcode.com/2023/day/8
 
-
 def main():
 
-    with open('day8/day8_2.txt') as input:
+    with open('day8/day8_3.txt') as input:
         inputLines = input.readlines()
         
     turns = inputLines[0][:-1]
 
     nodes = {}
+    part2ActiveNodes = []
     
     for line in inputLines[2:]:
         lineData = line.split(" = ")
         nodeName = lineData[0]
 
+        if nodeName.endswith('A'):
+            part2ActiveNodes.append(nodeName)
+
         rightAndLeft = lineData[1].split(', ')
         left = rightAndLeft[0][1:]
-        right = rightAndLeft[1][:-2]
+        right = rightAndLeft[1].replace(')', '').strip()
 
         nodes[nodeName] = (left, right)
 
+    #part 1
+    # ZZZfound = False
+    # currentNode = 'AAA'
+    # steps = 0
+
+    # while ZZZfound == False:
+
+    #     for turn in turns:
+    #         if turn == 'L':
+    #             currentNode = nodes[currentNode][0]
+    #         else:
+    #             currentNode = nodes[currentNode][1]
+
+    #         steps += 1
+
+    #         if currentNode == 'ZZZ':
+    #             ZZZfound = True
+    #             break
+
+    # print(f'Steps: {steps}')
+
+    #part 2
 
     ZZZfound = False
-    currentNode = 'AAA'
     steps = 0
 
     while ZZZfound == False:
 
         for turn in turns:
+            ZZZfound = True
+            activeNodesTemp = []
             if turn == 'L':
-                currentNode = nodes[currentNode][0]
+                for node in part2ActiveNodes:
+                    activeNodesTemp.append(nodes[node][0])
+                    if node.endswith('Z') == False:
+                        ZZZfound = False
             else:
-                currentNode = nodes[currentNode][1]
+                for node in part2ActiveNodes:
+                    activeNodesTemp.append(nodes[node][1])
+                    if node.endswith('Z') == False:
+                        ZZZfound = False
 
-            steps += 1
-
-            if currentNode == 'ZZZ':
-                ZZZfound = True
+            if ZZZfound == True:
                 break
 
-    print(f'Steps: {steps}')
+            steps += 1
+            part2ActiveNodes = activeNodesTemp
+            print(f'{part2ActiveNodes}')
+            
+    print(f'Part 2 steps: {steps}')
     
 if __name__ == '__main__':
     main()
