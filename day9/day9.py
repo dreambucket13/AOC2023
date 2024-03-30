91ef1e53827c376f85a1ff1febc7a95e3d2e099a
dreambucket13 <carmichr@gmail.com>
2024-03-29 21:33:29 -0400

    day9 part 1

--- day9/day9.py
+++ day9/day9.py
@@ -2,7 +2,7 @@
 
 def main():
 
-    with open('day9/day9_0.txt') as input:
+    with open('day9/day9_1.txt') as input:
         inputLines = input.readlines()
 
     histories = []
@@ -10,8 +10,44 @@ def main():
     for line in inputLines:
         histories.append(list(map(int, line.split(' '))))
     
-    pass
+    totalExtrapolatedForward = 0
 
-            
+    for history in histories:
+        totalExtrapolatedForward += extrapolateForward(history)
+
+    print(totalExtrapolatedForward)
+
+def extrapolateForward(history):
+
+    deltaLines = []
+    allZero = False
+    currentLine = history
+    deltaLines.append(history)
+
+    while allZero == False:
+
+        allZero = True
+        deltas = []
+
+        for index, value in enumerate(currentLine):
+            if index == 0:
+                continue
+            delta = value - currentLine[index - 1]
+            deltas.append(delta)
+
+            if delta != 0:
+                allZero = False
+        
+        deltaLines.append(deltas)
+
+        currentLine = deltaLines[len(deltaLines) - 1]
+
+    extrapolated = 0
+
+    for line in reversed(deltaLines):
+        extrapolated += line[-1]
+
+    return extrapolated
+    
 if __name__ == '__main__':
     main()
