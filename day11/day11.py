d0e86c20b7bd9743ecd93a3acc00ad5b35ebcaf0
dreambucket13 <carmichr@gmail.com>
2024-04-18 22:08:16 -0400

    day 11 part 2

--- day11/day11.py
+++ day11/day11.py
@@ -79,10 +79,83 @@ def part1(file):
 
             checked[(galaxy1, galaxy2)] = True
             checked[(galaxy2, galaxy1)] = True
-            
-    print(totalDistance)
+
+    print(f'part1: {totalDistance}')
     return totalDistance
+
+def part2(file, expansionFactor):
+
+    with open(file) as input:
+        inputLines = input.readlines()
+
+    galaxies = []
+    rowsToExpand = []
+    colsToExpand = [x for x in range(0,len(inputLines[0])-1)]
+
+    for row, line in enumerate(inputLines):
+        expandRow = True
+        for col, character in enumerate(line):
+            if character == '#':
+                galaxies.append( [row, col] )
+                expandRow = False
+                
+                if col in colsToExpand:
+                    colsToExpand.remove(col)
+
+        if expandRow == True:
+            rowsToExpand.append(row)
+
+    #expand the galaxies.  rows shift galaxies after them to the right.
+    # cols shift galaxies below them down (i.e., increase their row num)
+
+    for galaxy in galaxies:
+        colExpansion = 0
+        rowExpansion = 0
+
+        for col in colsToExpand:
+
+            galaxyX = galaxy[1]
+            if galaxyX > col:
+                colExpansion += expansionFactor - 1
+
+        galaxy[1] += colExpansion
+
+        for row in rowsToExpand:
+            galaxyY = galaxy[0]
+            if galaxyY > row:
+                rowExpansion += expansionFactor - 1
+
+        galaxy[0] += rowExpansion
+        
+    # distance = x delta + y delta
+
+    totalDistance = 0
+    checked = {}
+
+    # convert galaxies to tuples so I can hash them.
+
+    galaxies = list(map(tuple, galaxies))
+
+    for galaxy1 in galaxies:
+        for galaxy2 in galaxies:
+
+            if galaxy1 == galaxy2 or (galaxy1, galaxy2) in checked:
+                continue
+            
+            totalDistance += abs(galaxy2[0]-galaxy1[0]) + abs(galaxy2[1]-galaxy1[1])
+
+            checked[(galaxy1, galaxy2)] = True
+            checked[(galaxy2, galaxy1)] = True
+            
+    print(f'part 2: {totalDistance}')
+    return totalDistance
+
 
 if __name__ == '__main__':
     assert part1('day11\day11_0.txt') == 374
     assert part1('day11\day11_1.txt') == 9214785
+
+    assert part2('day11\day11_0.txt', 10) == 1030
+    assert part2('day11\day11_0.txt', 100) == 8410
+
+    part2('day11/day11_1.txt', 1000000)
