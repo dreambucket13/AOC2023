b2bc3d8c0f44dfb9c260b924db526fdf5cdb6ec1
dreambucket13 <carmichr@gmail.com>
2024-04-21 21:14:11 -0400

    isValidPlacement testing well

--- day12/day12.py
+++ day12/day12.py
@@ -1,21 +1,85 @@
 # https://adventofcode.com/2023/day/12
 
+class QuestionableSection:
+    def __init__ (self, start, length):
+        self.start = start
+        self.length = length
+        self.end = start + length - 1
+
 class SpringRecord:
     def __init__(self, conditionRecord, confirmedDamaged):
         self.conditionRecord = conditionRecord
         self.confirmedDamaged = confirmedDamaged
 
-def analyzeRecord(springRecord: SpringRecord) -> int:
+        inSection = False
+        self.questionableSections = []
+
+        for index, char in enumerate(conditionRecord):
+            if char == '?' and inSection == False:
+                inSection = True
+                start = index
+            elif (char != '?' or index == len(conditionRecord)-1) and inSection == True:
+                inSection = False
+                length = index - start
+                self.questionableSections.append(QuestionableSection(start, length))
+
+def isValidPlacement(conditionRecord, start, damagedSectionlength):
+
+
+    foundSectionLength = 0
+    index = start
+    char = conditionRecord[index]
+
+    if start + damagedSectionlength > len(conditionRecord):
+        return False
+    elif char not in ('?', '#'):
+        return False
+    elif damagedSectionlength < 1:
+        return False
+
+
+    sectionEnd = False
+
+    while sectionEnd == False and index < len(conditionRecord):
+        if char == '?':
+
+            if foundSectionLength >= damagedSectionlength:
+                sectionEnd = True
+                break
+
+            foundSectionLength += 1
+            index += 1
+        elif char == '#':
+            foundSectionLength += 1
+            index += 1 
+        elif char == '.' or index + 1 >= len(conditionRecord):
+            sectionEnd = True
+
+        char = conditionRecord[index]
+
+    return foundSectionLength == damagedSectionlength
+
+           
+
+def analyzeRecord(springRecord: SpringRecord) -> dict:
     combinations = 0
 
-    unknownGroups = []
+    conditionRecord = springRecord.conditionRecord
+    confirmedDamagedSectionLength  = springRecord.confirmedDamaged[0]
 
-    confirmedGroups = []
+    usedPositions = [[] for damagedSection in springRecord.confirmedDamaged]
 
-    #first, determine which groups are confirmed?
+    index = 0
+    foundAllCombinations = False
 
-    for spring in springRecord.conditionRecord:
-        pass
+
+
+
+    # while foundAllCombinations == False:
+    #     pass
+
+
+
 
     return combinations
 
@@ -39,6 +103,15 @@ def part1(file):
         springRecords.append(SpringRecord(data[0], confirmedDamaged))
 
     validCombinations = 0
+
+    assert isValidPlacement('.??..??...?##.', 10, 3) == True
+    assert isValidPlacement('.??..??...?##.', 10, 5) == False
+    assert isValidPlacement('.??..??...?##.', 10, 4) == False
+    assert isValidPlacement('.??..??...?##.', 1, 1) == True
+    assert isValidPlacement('.??..??...?##.', 2, 1) == True
+    assert isValidPlacement('.??..??...?##.', 0, 1) == False
+    assert isValidPlacement('?###????????', 0, 3 ) == False
+    assert isValidPlacement('?###????????', 0, 4 ) == True
 
     for springRecord in springRecords:
         validCombinations += analyzeRecord(springRecord)
